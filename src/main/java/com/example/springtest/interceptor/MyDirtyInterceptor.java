d9de7d88973abb0e7a42aff066bb9b4b02772869
nikita <hekutok@gmail.com>
2024-11-30 12:25:00 +0300

    add onterceptor

---
+++ src/main/java/com/example/springtest/interceptor/MyDirtyInterceptor.java
@@ -0,0 +1,24 @@
+package com.example.springtest.interceptor;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MyDirtyInterceptor implements HandlerInterceptor{
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            @Nullable ModelAndView modelAndView) throws Exception {
+                System.out.println("THIS IS POST HANDLE INTERCEPTOR CALLING");
+    }
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        System.out.println("THIS IS POST HANDLE INTERCEPTOR CALLING");
+                return true;
+    }
+}

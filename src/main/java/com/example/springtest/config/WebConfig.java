08323e19bc56800d0aaff0b8681a6a727310f7bc
nikita <hekutok@gmail.com>
2024-11-30 12:28:57 +0300

    interceptor add fix

--- src/main/java/com/example/springtest/config/WebConfig.java
+++ src/main/java/com/example/springtest/config/WebConfig.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.example.springtest.interceptor.MyDirtyInterceptor;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new MyCustomInterceptor());
+        registry.addInterceptor(new MyDirtyInterceptor());
     }
 }
 
